f8e912494577af77a77fa94722c1645dd1cb38d0
xeikh11 <51857159+xeikh11@users.noreply.github.com>
2020-08-21 22:21:03 +0500

    Finished

---
+++ app/Exports/ExportData.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace App\Exports;
+
+// use App\ExportData;
+use Maatwebsite\Excel\Concerns\FromArray;
+use App\Imports\ImportUsers;
+use Exception;
+use Maatwebsite\Excel\Concerns\WithCustomStartCell;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\RegistersEventListeners;
+use Maatwebsite\Excel\Concerns\WithEvents;
+use Maatwebsite\Excel\Events\AfterSheet;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use PhpOffice\PhpSpreadsheet\Style\Fill;
+
+use Exportable;
+// WithCustomStartCell
+class ExportData implements FromArray, ShouldAutoSize, WithEvents
+{
+
+    private function getNameFromNumber($num)
+    {
+        $numeric = ($num - 1) % 26;
+        $letter = chr(65 + $numeric);
+        $num2 = intval(($num - 1) / 26);
+
+        if ($num2 > 0) {
+            return $this->getNameFromNumber($num2) . $letter;
+        } else {
+            return $letter;
+        }
+    }
+
+    public function registerEvents(): array
+    {
+        return [
+            AfterSheet::class    => function (AfterSheet $event) {
+                $rows = $event->sheet->getDelegate()->toArray();
+                $event->sheet->getStyle('A1')->applyFromArray([
+
+                    'fill' => [
+                        'fillType' => Fill::FILL_SOLID,
+                        'color' => [
+                            'argb' => 'FFF9EF5B'
+                        ]
+                    ]
+                ]);
+                $event->sheet->getStyle('A4')->applyFromArray([
+
+                    'fill' => [
+                        'fillType' => Fill::FILL_SOLID,
+                        'color' => [
+                            'argb' => 'FFF9EF5B'
+                        ]
+                    ]
+                ]);
+                $event->sheet->getStyle('B1:H1')->applyFromArray([
+
+                    'fill' => [
+                        'fillType' => Fill::FILL_SOLID,
+                        'color' => [
+                            'argb' => 'FFF7AE5B'
+                        ]
+                    ]
+                ]);
+                // dd(count($this->domainDetailArray);
+                $event->sheet->getStyle('B4:H4')->applyFromArray([
+
+                    'fill' => [
+                        'fillType' => Fill::FILL_SOLID,
+                        'color' => [
+                            'argb' => 'FF85EC49'
+                        ]
+                    ]
+                ]);
+
+                // dd($shee);
+                // $event->sheet->setOrientation(\PhpOffice\PhpSpreadsheet\Worksheet\PageSetup::ORIENTATION_LANDSCAPE);
+
+                // $event->sheet->getParent()->getDefaultStyle()->applyFromArray([
+                //     'font' => [
+                //         'name' => 'Arial',
+                //         'size' => 42
+                //     ]
+                // ]);
+                // for($key = 1; $key < ($this->NoofDays * 3) + 1; $key++) {
+                //     $letter = $this->getNameFromNumber($key + 1);
+                //     if($key > 0){
+                //         for($i = 2; $i < $this->usersCount + 4; $i++)
+                //         {
+                //             if($key%3 == 1){
+                //                 // dd($letter.$i);
+                //                 $event->sheet->getStyle($letter.$i.':'.$letter.$i)->applyFromArray([
+
+                //                     'fill' => [
+                //                         'fillType' => Fill::FILL_SOLID,
+                //                         'color' => [
+                //                             'argb' => 'FFF9EF5B'
+                //                         ]
+                //                     ]
+                //                 ]);
+                //             }
+                //             if($key%3 == 2){
+
+                //                 $event->sheet->getStyle($letter.$i.':'.$letter.$i)->applyFromArray([
+
+                //                     'fill' => [
+                //                         'fillType' => Fill::FILL_SOLID,
+                //                         'color' => [
+                //                             'argb' => 'FF3FA5F2'
+                //                         ]
+                //                     ]
+                //                 ]);
+
+                //             }
+                //             if($key%3 == 0){
+
+                //                 $event->sheet->getStyle($letter.$i.':'.$letter.$i)->applyFromArray([
+
+                //                     'fill' => [
+                //                         'fillType' => Fill::FILL_SOLID,
+                //                         'color' => [
+                //                             'argb' => 'FFB0FA7C'
+                //                         ]
+                //                     ]
+                //                 ]);
+                //             }
+                //         }
+                //     }
+                // }
+            },
+        ];
+    }
+
+    public static function afterSheet(AfterSheet $event)
+    {
+        dd("sdkfgsdhf");
+        $cellRange = 'A1:W1';
+        $event->sheet->getDelegate()->getStyle($cellRange)->getFont()->setSize(34);
+    }
+
+    public function __construct($domainDetailArray, $ipDetailArray)
+    {
+        $this->domainDetailArray = $domainDetailArray;
+        $this->ipDetailArray = $ipDetailArray;
+        return $this;
+    }
+    /**
+     * @return \Illuminate\Support\Collection
+     */
+    // public function collection()
+    // {
+    //     // dd($this->usersCount);
+
+    //     return ExportData::all();
+    // }
+
+    public function array(): array
+    {
+
+        $output = [];
+        $domainHead = [];
+        $domainHead = ['Domain', 'Country', 'Owner', 'Registrant Org', 'SPAM', 'Created on', 'ISP', 'IP'];
+        $ipHead = ['IP', 'Country', 'Owner', 'Registrant Org', 'SPAM', 'Created on', 'ISP', 'Domain'];
+        array_push($output, $domainHead);
+        array_push($output, $this->domainDetailArray);
+        array_push($output, [' ']);
+        array_push($output, $ipHead);
+        array_push($output, $this->ipDetailArray);
+
+        // dd($output);
+        return $output;
+    }
+}
--- app/Http/Controllers/LookupDataContoller.php
+++ app/Http/Controllers/LookupDataContoller.php
@@ -1,22 +1,82 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use Iodev\Whois\Factory;
+use Maatwebsite\Excel\Facades\Excel;
+use App\Exports\ExportData;
+use phpWhois\Whois;
+use ipinfo\ipinfo\IPinfo;
 
 use Illuminate\Http\Request;
 
 class LookupDataContoller extends Controller
 {
     //
-    public function extractData(Request $request){
+    public function extractData(Request $request)
+    {
 
         // dd($request->all());
-        $whois = Factory::get()->createWhois();
-        $response = $whois->loadDomainInfo("fiverr.com");
-        $ip = gethostbyname("google.com");
+        if (isset($request->domain)) {
+            $domainName = $request->domain;
+            $whois = Factory::get()->createWhois();
+            // $ip_address = '216.58.209.142';
+            // dd($whois->lookupDomain("google.com"));
+            try {
 
-        // $response = $whois->loadDomainInfo($domain);
-        dd($response);
+                $domainData = $whois->loadDomainInfo($domainName);
+                $c_name = $domainData->getExtra()['groups'][0]['Registrant Country'];
+                $owner = $domainData->getExtra()['groups'][0]['Registrar'];
+                $org = $domainData->getExtra()['groups'][0]['Registrant Organization'];
+                $spam = $domainData->getExtra()['groups'][0]['Registrar Abuse Contact Email'];
+                $created_on = $domainData->getExtra()['groups'][0]['Creation Date'];
+                $isp = $domainData->getExtra()['groups'][0]['Name Server'][0];
+                $ip = gethostbyname($domainName);
+                // dd(gethostbyaddr('199.7.202.92'));
+                // get country full name
+                $names = json_decode(file_get_contents("http://country.io/names.json"), true);
+                $country = $names[$c_name];
+                // $domainDetailArray = [];
 
+                $domainDetailArray = ['domain' => $domainName, 'country' => $country, 'owner' => $owner, 'org' => $org, 'spam' => $spam, 'created_on' => $created_on, 'isp' => $isp,  'ip' => $ip];
+            } catch (\Exception $e) {
+                $domainDetailArray = [' '];
+            }
+
+
+            // $domainIP = gethostbyname("oplayblueprints.com");
+            // dd($domainData->getExtra());
+            // $extra = $domainData->getExtra();
+            // dd($domainData->getExtra()['groups'][0]['Name Server'][0]);
+
+
+
+        }
+
+        if (isset($request->ip)) {
+            $IP = $request->ip;
+            $whois = new Whois();
+            $result = $whois->lookup($request->ip);
+            // dd($result);
+            if (!isset($result['regrinfo']['owner'])) {
+                $IPDetailArray = [' '];
+            } else {
+                $i_country = $result['regrinfo']['owner']['address']['country'];
+                $names = json_decode(file_get_contents("http://country.io/names.json"), true);
+                $ip_country = $names[$i_country];
+                $ip_owner = $result['regrinfo']['owner']['organization'];
+                $ip_org = $result['regrinfo']['owner']['organization'];
+                $ip_spam = $result['regrinfo']['abuse']['email'];
+                $ip_isp = $result['regrinfo']['network']['name'];
+                $ip_created = $result['regrinfo']['network']['created'];
+                $ip_domain = gethostbyaddr($request->ip);
+                $IPDetailArray = [];
+                $IPDetailArray = [
+                    'ip' => $IP, 'country' => $ip_country, 'owner' => $ip_owner, 'org' => $ip_org, 'spam' => $ip_spam,
+                    'created_on' => $ip_created, 'isp' => $ip_isp,  'domain' => $ip_domain
+                ];
+            }
+        }
+        return Excel::download(new ExportData($domainDetailArray, $IPDetailArray), 'details.xls');
     }
 }
